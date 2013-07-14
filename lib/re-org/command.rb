dd8c0005624af5cc46dfbf611258d911df9e8627
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2013-07-15 02:00:33 +0900

    Added clockfile template

--- lib/re-org/command.rb
+++ lib/re-org/command.rb
@@ -9,20 +9,34 @@ module ReOrg
     end
 
     def execute!
-      case
-      when @options['new'] == true
+      case true
+      when @options['setup'] 
+        prepare_directories
+      when @options['new']
         new_file
-      when @options['completed'] == true
-        completed
+      when @options['everything']
+        reorganize_everything
       end
     end
 
     def new_file
-      prepare_run
+      prepare_directories
+
+      @org[:title]    = @options["--title"] || 'Untitled'
+      @org[:time]     = Time.now
+      @org[:type]     = @options["<template>"]      
+      @org[:filename] = Time.at(@org[:time]).strftime("#{fix_title(@org[:title])}-%Y-%m-%d-%s")
+      @org[:file]     = File.expand_path(File.join(@org[:current_dir], "#{@org[:filename]}.org"))
+
+      c = 1
+      while File.exists?(@org[:file])
+        c += 1
+        @org[:filename] = Time.at(@org[:time]).strftime("%Y-%m-%d-%s-#{c}")
+        @org[:file]     = File.expand_path(File.join(@org[:current_dir], "#{@org[:filename]}.org"))
+      end
 
       @org[:org_date] = Time.at(@org[:time]).strftime("[%Y-%m-%d %a]")
-      @org[:title] = @options["title"] || "Untitled"
-      template =  @options["<name>"] || 'writing'
+      template =  @options["<template>"] || 'writing'
 
       template_file = File.expand_path("templates/#{template}.org", File.dirname(__FILE__))
       if not File.exists?(template_file)
@@ -36,24 +50,19 @@ module ReOrg
       puts "Created a new writing at `#{@org[:file]}'".green
     end
 
-    def prepare_run
-      current_dir = File.expand_path('../current')
+    def fix_title(title)
+      return nil unless title
+      title.gsub(/[\s.\/\\]/, '-').downcase
+    end
+
+    def prepare_directories
+      current_dir = File.expand_path('./current', File.dirname('.'))
       if not File.exists?(current_dir)
-        puts "Creating current dir at `#{current_dir}'".green
+        puts "Creating working dir at `#{current_dir}'".green
         FileUtils.mkdir(current_dir)
       end
 
       @org[:current_dir] = current_dir
-      @org[:time]     = Time.now
-      @org[:filename] = Time.at(@org[:time]).strftime("%Y-%m-%d-%s")
-      @org[:file]     = File.expand_path(File.join(@org[:current_dir], "#{@org[:filename]}.org"))
-
-      c = 1
-      while File.exists?(@org[:file])
-        c += 1
-        @org[:filename] = Time.at(@org[:time]).strftime("%Y-%m-%d-%s-#{c}")
-        @org[:file]     = File.expand_path(File.join(@org[:current_dir], "#{@org[:filename]}.org"))
-      end
     end
   end
 end
