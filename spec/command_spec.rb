19847b7b16dfbb3be44f8ba7828fa44d05601970
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2013-08-18 16:41:30 +0900

    Fixing the tests a bit

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -19,18 +19,65 @@ describe ReOrg::Command do
     }
   end
 
-  it 'should create new writing with notebook and title' do
-    cmd['new']        = true
-    cmd['--notebook'] = 'new-notebook'
-    cmd['--title']    = 'a new title'
-    o = ReOrg::Command.new(cmd)
-    o.execute!
-    orgs = Dir["#{CURRENT_DIR}/*"]
-    orgs.count.should > 0
+  before(:each) do
+    FileUtils.mkdir_p("#{RESULTS_DIR}/current")
   end
 
-  it 'should setup the directories'
-  it 'should update a notebook folder from the contents of current/'
-  it 'should publish the contents from a notebook'
-  it 'should compile the contents from a notebook into a single file'
+  after(:each) do
+    `rm -rf #{RESULTS_DIR}/*`
+  end
+
+  context "when using `re-org new`" do 
+    before(:each) do
+      @cmd = {
+        "new"        => true,
+        "<template>" => 'writing',
+        "--notebook" => nil,
+        "--path"     => nil,
+        "--title"    => nil,
+      }
+    end    
+
+    it 'should create new writing for a notebook with --notebook option' do
+      @cmd['--notebook'] = 'tests'
+      o = ReOrg::Command.new(@cmd)
+      o.execute!
+      orgs = Dir["#{RESULTS_DIR}/*"]
+      orgs.count.should == 1
+    end
+
+    it 'should create new clockfile when the template is choosen' do
+      @cmd['<template>'] = 'clockfile'
+      @cmd['--notebook'] = 'tests'
+      o = ReOrg::Command.new(@cmd)
+      o.execute!
+      orgs = Dir["#{RESULTS_DIR}/*"]
+      orgs.count.should == 1
+    end
+
+    it 'should create new writing with --title option' do
+      @cmd['--notebook'] = 'tests'
+      @cmd['--title']    = 'Testing the title'
+      o = ReOrg::Command.new(@cmd)
+      o.execute!
+      orgs = Dir["#{RESULTS_DIR}/*"]
+      orgs.count.should == 1
+    end
+  end
+
+  context "when using `re-org setup`" do 
+    it 'should setup the directories'
+  end
+
+  context "when using `re-org update`" do
+    it 'should update a notebook folder from the contents of current/'
+  end
+
+  context "when using `re-org publish`" do
+    it 'should publish the contents from a notebook'
+  end
+
+  context "when using `re-org compile`" do
+    it 'should compile the contents from a notebook into a single file'
+  end
 end
