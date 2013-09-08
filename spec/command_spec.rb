6ac8c5bed3e0de461ec86a053d36618d9bde49b3
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2013-09-08 19:11:18 +0900

    Remove features

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,48 +1,31 @@
 require 'spec_helper'
 
 describe ReOrg::Command do
-  let(:cmd) do
-    {
-      "new"        => false,
-      "<template>" => nil,
-      "--notebook" => nil,
-      "--path"     => nil,
-      "--title"    => nil,
-      "setup"      => false,
-      "status"     => false,
-      "update"     => false,
-      "--force"    => false,
-      "compile"    => false,
-      "publish"    => false,
-      "--help"     => false,
-      "--version"  => false
-    }
-  end
+  before(:each) { FileUtils.mkdir_p("#{RESULTS_DIR}/current") }
 
-  before(:each) do
-    FileUtils.mkdir_p("#{RESULTS_DIR}/current")
-  end
-
-  after(:each) do
-    `rm -rf #{RESULTS_DIR}/*`
-  end
+  # Clean up after tests are done
+  after(:each)  { `rm -rf #{RESULTS_DIR}` }
 
   context "when using `re-org new`" do 
+    before(:all) do
+      @test_number = 0
+    end
+
     before(:each) do
       @cmd = {
         "new"        => true,
         "<template>" => 'writing',
         "--notebook" => nil,
-        "--path"     => nil,
-        "--title"    => nil,
+        "--title"    => "test-#{@test_number}",
       }
-    end    
+      @test_number += 1
+    end
 
     it 'should create new writing for a notebook with --notebook option' do
       @cmd['--notebook'] = 'tests'
       o = ReOrg::Command.new(@cmd)
       o.execute!
-      orgs = Dir["#{RESULTS_DIR}/*"]
+      orgs = Dir["#{RESULTS_DIR}/todo/*"]
       orgs.count.should == 1
     end
 
@@ -51,7 +34,7 @@ describe ReOrg::Command do
       @cmd['--notebook'] = 'tests'
       o = ReOrg::Command.new(@cmd)
       o.execute!
-      orgs = Dir["#{RESULTS_DIR}/*"]
+      orgs = Dir["#{RESULTS_DIR}/todo/*"]
       orgs.count.should == 1
     end
 
@@ -60,24 +43,8 @@ describe ReOrg::Command do
       @cmd['--title']    = 'Testing the title'
       o = ReOrg::Command.new(@cmd)
       o.execute!
-      orgs = Dir["#{RESULTS_DIR}/*"]
+      orgs = Dir["#{RESULTS_DIR}/todo/*"]
       orgs.count.should == 1
     end
-  end
-
-  context "when using `re-org setup`" do 
-    it 'should setup the directories'
-  end
-
-  context "when using `re-org update`" do
-    it 'should update a notebook folder from the contents of current/'
-  end
-
-  context "when using `re-org publish`" do
-    it 'should publish the contents from a notebook'
-  end
-
-  context "when using `re-org compile`" do
-    it 'should compile the contents from a notebook into a single file'
   end
 end
